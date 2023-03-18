df62fdd002c7f41663ddbe442a6031c6fc0d3a25
berserkPikachu <anirlearn1756@gmail.com>
2023-03-18 19:05:06 +0530

    Initial commit

---
+++ nvim/basic.ps1
@@ -0,0 +1,13 @@
+
+# Neovim
+
+rm -r $Env:USERPROFILE\AppData\Local\nvim
+# cd $Env:USERPROFILE\AppData\Local\nvim
+git clone https://github.com/awesomeDev12/nvim.git $Env:USERPROFILE\AppData\Local\nvim
+
+# Ultisnips
+
+rm -r $Env:USERPROFILE\AppData\Local\coc\ultisnips
+# cd $Env:USERPROFILE\AppData\Local\coc\ultisnips
+git clone https://github.com/awesomeDev12/ultisnips.git $Env:USERPROFILE\AppData\Local\coc\ultisnips
+
